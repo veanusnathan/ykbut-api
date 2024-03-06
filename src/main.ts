854aee558b6084e6df7d100281a1d2b97a17a5de
Veanus Nathan <veanusnathan.work@gmail.com>
2024-03-06 13:26:27 +0700

    add ip address to cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,14 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: [/http:\/\/localhost/, 'https://ykbut-dashboard.netlify.app/'],
+    origin: [
+      /http:\/\/localhost/,
+      'https://ykbut-dashboard.netlify.app/',
+      'http://62.72.27.168',
+      'http://104.43.65.128',
+      'http://62.72.27.168:1212',
+      'http://104.43.65.128:8086',
+    ],
   });
 
   app.enableShutdownHooks();
