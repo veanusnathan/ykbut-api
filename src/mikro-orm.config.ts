09f5eb0216527c806bec725f0dacf31f43eea419
hanif-faiq <haniffaiq95@gmail.com>
2024-03-22 11:47:47 +0700

    Update mikro-orm.config.ts

--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -2,11 +2,11 @@ import { TsMorphMetadataProvider } from '@mikro-orm/reflection';
 import { defineConfig } from '@mikro-orm/postgresql';
 
 export default defineConfig({
-  host: 'lama2dingin.id',
+  host: '137.116.147.25',
   port: 5432,
-  user: 'ykbutdb',
-  password: 'admin',
-  dbName: 'ykbutdb',
+  user: 'odoo',
+  password: 'odoo',
+  dbName: 'YKBUT_NEW_PROD_1',
   entities: ['./dist/**/*.entity.js', './dist/*.entity.js'],
   entitiesTs: ['./src/**/*.entity.ts', './src/*.entity.ts'],
   metadataProvider: TsMorphMetadataProvider,
