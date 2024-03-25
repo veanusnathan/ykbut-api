d33a69c8e42c0c18dfa1bffbc57f0ffa23168fd3
hanif-faiq <haniffaiq95@gmail.com>
2024-03-25 15:45:09 +0700

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,15 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: [
-      /http:\/\/localhost/,
-      'https://ykbut-dashboard.netlify.app/',
-      'http://62.72.27.168',
-      'http://104.43.65.128',
-      'http://62.72.27.168:1212',
-      'http://104.43.65.128:8086',
-      'http://137.116.147.25:5432',
-    ],
+    origin: '*',
   });
 
   app.enableShutdownHooks();
